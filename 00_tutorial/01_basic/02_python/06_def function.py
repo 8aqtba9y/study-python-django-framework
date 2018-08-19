c474c00a76e4a26af12ad2e8cf9b66ac20bff0ae
8aqtba9y <8aqtba9y@gmail.com>
2018-08-20 03:30:29 +0900

    [add] def function

---
+++ 00_tutorial/01_basic/02_python/06_def function.py
@@ -0,0 +1,18 @@
+def hi():
+    print("Hello world")
+
+hi()
+# Hello world
+
+
+def hello(name):
+    if name == "Ola":
+        print("Hello, Ola")
+    elif name == "Son":
+        print("Hello, Son")
+    else:
+        print("Hello, ",name)
+
+hello("Ola")
+# Hello, Ola
+
